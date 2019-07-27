b16ad8d30e5577ae9251c6b26471a157c8541626
Ertugrul Karademir <ertugrul.karademir@oliverwyman.com>
2019-07-27 15:23:21 +0100

    Moved socket binding to fixture

--- tests/functional/test_utils_func.py
+++ tests/functional/test_utils_func.py
@@ -1,12 +1,18 @@
 import socket
 
+import pytest
+
 from pymongo_inmemory import _utils
 
 
-def test_find_open_port2():
+@pytest.fixture
+def server():
     with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as server:
         server.setblocking(0)
         server.bind(("localhost", 12323))
         server.listen(5)
+        yield server
 
-        assert _utils.find_open_port([12323, 12324]) == 12324
+
+def test_find_open_port(server):
+    assert _utils.find_open_port([12323, 12324]) == 12324
