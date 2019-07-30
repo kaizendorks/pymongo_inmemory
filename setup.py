9655b42ea8a86d6ebe45c3af8b708590a8a876d0
Ertugrul Karademir <ertugrul.karademir@oliverwyman.com>
2019-07-30 20:13:44 +0100

    Added classifiers

--- setup.py
+++ setup.py
@@ -14,5 +14,16 @@ setup(
     author_email="kaizendorks@gmail.com",
     license="MIT",
     packages=find_packages(exclude=["tests", "tests.*", ".vscode"]),
-    install_requires=required
+    install_requires=required,
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Development Status :: 3 - Alpha",
+        "Intended Audience :: Developers",
+        "Operating System :: MacOS :: MacOS X",
+        "Operating System :: POSIX :: Linux",
+        "Topic :: Database :: Database Engines/Servers",
+        "Topic :: Software Development :: Testing",
+        "Topic :: Utilities",
+    ],
 )
