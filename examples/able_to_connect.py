5ae8128fb18a1e3473949189df898fa04c1d5526
Ertugrul Karademir <ekarademir@gmail.com>
2020-05-30 21:20:54 +0100

    Added few more tests for running in parallel

--- examples/able_to_connect.py
+++ examples/able_to_connect.py
@@ -16,3 +16,4 @@ if __name__ == "__main__":
         inserted_id = collection.insert_one(data).inserted_id
         mongo_dump = bson.decode(client.pim_mongodump("test-db", "my-collection"))
         assert mongo_dump["some"] == "data"
+        assert mongo_dump["_id"] == inserted_id
