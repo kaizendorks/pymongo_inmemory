146f38891048ce7168f8685440649775f0662318
Ertugrul Karademir <ekarademir@gmail.com>
2021-03-04 21:12:06 +0000

    Ability to override mongod port (#36)
    * Added ability to override port
    * Updated README to clarify usage
    * Added a test for port override behaviour
    * Updated integrity test running
    * Updated actions and ignoring `.venv` for linting tests

--- tests/integrity/test_integrity.sh
+++ tests/integrity/test_integrity.sh
@@ -1,13 +1,18 @@
 #!/bin/bash
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
-# echo $DIR
-bash $DIR/test_mongod_cleanup.sh
 
+bash $DIR/test_mongod_cleanup.sh
 RESULT=$?
 if [ $RESULT == 0 ]; then
-  echo "All integrity tests successful."
-  exit 0
-else
-  echo "Some tests failed."
-  exit 1
+  bash $DIR/test_mongod_port_override.sh
+  RESULT=$?
+  if [ $RESULT == 0 ]; then
+    echo "============================================================================================="
+    echo "All integrity tests successful."
+    exit 0
+  fi
 fi
+
+echo "============================================================================================="
+echo "Some tests failed."
+exit 1
--- tests/integrity/test_mongod_port_override.sh
+++ tests/integrity/test_mongod_port_override.sh
@@ -1,15 +1,16 @@
 #!/bin/bash
 
-# test_mongod_cleanup.sh
+# test_mongod_port_override.sh
 #
-# This test checks if the main python process can properly clean up after being
-# terminated.
+# This test checks if we can override cound mongod port with an environmental variable.
 
 echo "============================================================================================="
-echo "  Starting clean up test."
+echo "  Starting port override test."
 echo "---------------------------------------------------------------------------------------------"
 
-WAIT_TO_KILL=30
+export PYMONGOIM__MONGOD_PORT=27019
+
+WAIT_FOR_SPINUP=10
 WAIT_FOR_SPINDOWN=2
 
 MONGOD_BEFORE=$(pgrep mongod)
@@ -17,21 +18,25 @@ echo "All mongod processes running before we kick off: $MONGOD_BEFORE"
 
 $(which python3) -m pymongo_inmemory.mongod > /dev/null &
 PYTHON_PROCESS_ID=$!
-echo "Started python process with PID $PYTHON_PROCESS_ID. Waiting to kill in $WAIT_TO_KILL seconds."
-sleep $WAIT_TO_KILL
+echo "Started python process with PID $PYTHON_PROCESS_ID. Waiting $WAIT_FOR_SPINUP seconds to allow bootup."
+sleep $WAIT_FOR_SPINUP
+echo "Checking bound port"
+lsof -iTCP -sTCP:LISTEN | grep mongod | grep $PYMONGOIM__MONGOD_PORT
+RESULT=$?
 echo "Killing python process, $PYTHON_PROCESS_ID"
 kill $PYTHON_PROCESS_ID
 echo "Waiting, $WAIT_FOR_SPINDOWN, to allow shutoff"
 sleep $WAIT_FOR_SPINDOWN
+unset PYMONGOIM__MONGOD_PORT
 
 MONGOD_AFTER=$(pgrep mongod)
 echo "All mongod processes running after we kill main thread: $MONGOD_AFTER"
 
 echo "---------------------------------------------------------------------------------------------"
-if [ "$MONGOD_BEFORE" == "$MONGOD_AFTER" ]; then
-  echo "Clean up test successful."
-  exit 0
-else
-  echo "Clean up test failed!"
+if [ $RESULT == 1 ]; then
+  echo "Port override failed!"
   exit 1
+else
+  echo "Port override successful"
+  exit 0
 fi
