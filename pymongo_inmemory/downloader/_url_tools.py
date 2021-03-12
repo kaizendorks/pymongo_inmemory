56757ec0c46b62f24f37a093dea161acce4db69d
Ertugrul Karademir <ekarademir@gmail.com>
2021-03-12 21:41:46 +0000

    Added all versions

--- pymongo_inmemory/downloader/_url_tools.py
+++ pymongo_inmemory/downloader/_url_tools.py
@@ -101,7 +101,7 @@ URLS = {
         "patches": list(range(23)),
         "os_names": {
             "windows": {
-                "generic": "https://fastdl.mongodb.org/win32/mongodb-win32-x86_64-2008plus-{}.zip"
+                "generic": "https://fastdl.mongodb.org/win32/mongodb-win32-x86_64-2008plus-{}.zip",
             },
             "ubuntu": {
                 "18": "https://fastdl.mongodb.org/linux/mongodb-linux-x86_64-ubuntu1804-{}.tgz",
@@ -125,6 +125,109 @@ URLS = {
             "debian": {
                 "8": "https://fastdl.mongodb.org/linux/mongodb-linux-x86_64-debian81-{}.tgz",
                 "9": "https://fastdl.mongodb.org/linux/mongodb-linux-x86_64-debian92-{}.tgz",
+            },
+            "amazon": {
+                "1": "https://fastdl.mongodb.org/linux/mongodb-linux-x86_64-amazon-{}.tgz",
+                "2": "https://fastdl.mongodb.org/linux/mongodb-linux-x86_64-amazon2-{}.tgz",
+            }
+        }
+    },
+    MajorMinor(4, 0): {
+        "patches": list(range(24)),
+        "os_names": {
+            "windows": {
+                "generic": "https://fastdl.mongodb.org/win32/mongodb-win32-x86_64-2008plus-{}.zip",
+            },
+            "ubuntu": {
+                "18": "https://fastdl.mongodb.org/linux/mongodb-linux-x86_64-ubuntu1804-{}.tgz",
+                "16": "https://fastdl.mongodb.org/linux/mongodb-linux-x86_64-ubuntu1604-{}.tgz",
+                "14": "https://fastdl.mongodb.org/linux/mongodb-linux-x86_64-ubuntu1404-{}.tgz",
+            },
+            "suse": {
+                "12": "https://fastdl.mongodb.org/linux/mongodb-linux-x86_64-suse12-{}.tgz"
+            },
+            "rhel": {
+                "8": "https://fastdl.mongodb.org/linux/mongodb-linux-x86_64-rhel80-{}.tgz",
+                "6": "https://fastdl.mongodb.org/linux/mongodb-linux-x86_64-rhel62-{}.tgz",
+                "7": "https://fastdl.mongodb.org/linux/mongodb-linux-x86_64-rhel70-{}.tgz",
+            },
+            "osx": {
+                "generic": "https://fastdl.mongodb.org/osx/mongodb-osx-ssl-x86_64-{}.tgz",
+            },
+            "linux": {
+                "generic": "https://fastdl.mongodb.org/linux/mongodb-linux-x86_64-{}.tgz",
+            },
+            "debian": {
+                "8": "https://fastdl.mongodb.org/linux/mongodb-linux-x86_64-debian81-{}.tgz",
+                "9": "https://fastdl.mongodb.org/linux/mongodb-linux-x86_64-debian92-{}.tgz",
+            },
+            "amazon": {
+                "1": "https://fastdl.mongodb.org/linux/mongodb-linux-x86_64-amazon-{}.tgz",
+                "2": "https://fastdl.mongodb.org/linux/mongodb-linux-x86_64-amazon2-{}.tgz",
+            }
+        }
+    },
+    MajorMinor(4, 2): {
+        "patches": list(range(4)) + list(range(5,13)),
+        "os_names": {
+            "windows": {
+                "generic": "https://fastdl.mongodb.org/win32/mongodb-win32-x86_64-2012plus-{}.zip",
+            },
+            "ubuntu": {
+                "18": "https://fastdl.mongodb.org/linux/mongodb-linux-x86_64-ubuntu1804-{}.tgz",
+                "16": "https://fastdl.mongodb.org/linux/mongodb-linux-x86_64-ubuntu1604-{}.tgz",
+            },
+            "suse": {
+                "15": "https://fastdl.mongodb.org/linux/mongodb-linux-x86_64-suse15-{}.tgz",
+                "12": "https://fastdl.mongodb.org/linux/mongodb-linux-x86_64-suse12-{}.tgz",
+            },
+            "rhel": {
+                "8": "https://fastdl.mongodb.org/linux/mongodb-linux-x86_64-rhel80-{}.tgz",
+                "6": "https://fastdl.mongodb.org/linux/mongodb-linux-x86_64-rhel62-{}.tgz",
+                "7": "https://fastdl.mongodb.org/linux/mongodb-linux-x86_64-rhel70-{}.tgz",
+            },
+            "osx": {
+                "generic": "https://fastdl.mongodb.org/osx/mongodb-osx-ssl-x86_64-{}.tgz",
+            },
+            "linux": {
+                "generic": "https://fastdl.mongodb.org/linux/mongodb-linux-x86_64-{}.tgz",
+            },
+            "debian": {
+                "9": "https://fastdl.mongodb.org/linux/mongodb-linux-x86_64-debian92-{}.tgz",
+                "10": "https://fastdl.mongodb.org/linux/mongodb-linux-x86_64-debian10-{}.tgz",
+            },
+            "amazon": {
+                "1": "https://fastdl.mongodb.org/linux/mongodb-linux-x86_64-amazon-{}.tgz",
+                "2": "https://fastdl.mongodb.org/linux/mongodb-linux-x86_64-amazon2-{}.tgz",
+            }
+        }
+    },
+    MajorMinor(4, 4): {
+        "patches": list(range(5)),
+        "os_names": {
+            "windows": {
+                "generic": "https://fastdl.mongodb.org/windows/mongodb-windows-x86_64-{}.zip",
+            },
+            "ubuntu": {
+                "20": "https://fastdl.mongodb.org/linux/mongodb-linux-x86_64-ubuntu2004-{}.tgz",
+                "18": "https://fastdl.mongodb.org/linux/mongodb-linux-x86_64-ubuntu1804-{}.tgz",
+                "16": "https://fastdl.mongodb.org/linux/mongodb-linux-x86_64-ubuntu1604-{}.tgz",
+            },
+            "suse": {
+                "15": "https://fastdl.mongodb.org/linux/mongodb-linux-x86_64-suse15-{}.tgz",
+                "12": "https://fastdl.mongodb.org/linux/mongodb-linux-x86_64-suse12-{}.tgz",
+            },
+            "rhel": {
+                "8": "https://fastdl.mongodb.org/linux/mongodb-linux-x86_64-rhel80-{}.tgz",
+                "6": "https://fastdl.mongodb.org/linux/mongodb-linux-x86_64-rhel62-{}.tgz",
+                "7": "https://fastdl.mongodb.org/linux/mongodb-linux-x86_64-rhel70-{}.tgz",
+            },
+            "osx": {
+                "generic": "https://fastdl.mongodb.org/osx/mongodb-osx-ssl-x86_64-{}.tgz",
+            },
+            "debian": {
+                "9": "https://fastdl.mongodb.org/linux/mongodb-linux-x86_64-debian92-{}.tgz",
+                "10": "https://fastdl.mongodb.org/linux/mongodb-linux-x86_64-debian10-{}.tgz",
             },
             "amazon": {
                 "1": "https://fastdl.mongodb.org/linux/mongodb-linux-x86_64-amazon-{}.tgz",
