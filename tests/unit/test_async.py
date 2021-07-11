8030f20a3b4d00e069dddd229ad1750a2af90735
Hilario Fernandes <hilariofernandes@gmail.com>
2021-07-11 08:09:59 +0100

    Don't add signals if not in main thread (#40)

---
+++ tests/unit/test_async.py
@@ -0,0 +1,19 @@
+import asyncio
+import threading
+import sys
+
+def test_init_module_in_loop():
+    sys.modules.pop('pymongo_inmemory.mongod', None)
+
+    async def main():
+        import pymongo_inmemory.mongod
+        return True
+
+    loop = asyncio.new_event_loop()
+    thread = threading.Thread(target=loop.run_forever, daemon=True)
+    thread.start()
+    result = asyncio.run_coroutine_threadsafe(main(), loop).result()
+    loop.call_soon_threadsafe(loop.stop)
+    thread.join()
+
+    assert result
