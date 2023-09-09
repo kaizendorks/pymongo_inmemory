2008b18d463b527e4b5104de7697d4d2911a535e
Ertugrul Karademir <ekarademir@gmail.com>
2023-09-09 19:55:26 +0100

    Typos

--- pymongo_inmemory/context.py
+++ pymongo_inmemory/context.py
@@ -18,11 +18,11 @@ class OperatingSystemNotFound(ValueError):
     pass
 
 
-def _coercion(consturctor, value):
-    if consturctor == bool:
+def _coercion(constructor, value):
+    if constructor == bool:
         return value == "True"
     else:
-        return consturctor(value)
+        return constructor(value)
 
 
 def _check_environment_vars(option, fallback=None):
@@ -37,7 +37,7 @@ def _check_cfg(option, filename, fallback=None):
     return parser.get("pymongo_inmemory", option, fallback=fallback, raw=True)
 
 
-def conf(option, fallback=None, optional=True, ceorce_with=str):
+def conf(option, fallback=None, optional=True, coerce_with=str):
     """Retrieve asked `option` if possible. There are number of places that are checked.
     In the order of precedence,
     1. Environment variables
@@ -82,13 +82,13 @@ def conf(option, fallback=None, optional=True, ceorce_with=str):
             )
     else:
         try:
-            value = _coercion(ceorce_with, value)
+            value = _coercion(coerce_with, value)
         except ValueError:
             value = None
         except Exception:
             raise ValueError(
                 ("Can't coerce the value of {} to type {}").format(
-                    option, ceorce_with.__qualname__
+                    option, coerce_with.__qualname__
                 )
             )
 
@@ -102,7 +102,7 @@ class Context:
         self, os_name=None, version=None, os_ver=None, ignore_cache=False
     ) -> None:
         self.mongo_version = conf("mongo_version", version)
-        self.mongod_port = conf("mongod_port", None, ceorce_with=int)
+        self.mongod_port = conf("mongod_port", None, coerce_with=int)
 
         self.operating_system = self._build_operating_system_info(os_name)
         self.os_version = conf("os_version", os_ver)
@@ -113,8 +113,8 @@ class Context:
 
         self.url_hash = hashlib.sha256(bytes(self.download_url, "utf-8")).hexdigest()
 
-        self.ignore_cache = conf("ignore_cache", ignore_cache, ceorce_with=bool)
-        self.use_local_mongod = conf("use_local_mongod", False, ceorce_with=bool)
+        self.ignore_cache = conf("ignore_cache", ignore_cache, coerce_with=bool)
+        self.use_local_mongod = conf("use_local_mongod", False, coerce_with=bool)
 
         self.download_folder = conf(
             "download_folder", mkdir_ifnot_exist(CACHE_FOLDER, "download")
